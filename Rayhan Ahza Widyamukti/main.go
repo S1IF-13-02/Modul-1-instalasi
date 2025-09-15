fc221aeb25c054d36eb6183140ed4e11bc5bfcb6
Ehan <112927710+Ahzaaa16@users.noreply.github.com>
2025-09-15 17:19:41 +0700

    Add files via upload

---
+++ Rayhan Ahza Widyamukti/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Hasil Add(2, 3):", Add(2, 3))
+	fmt.Println("Hasil Add(-2, -3):", Add(-2, -3))
+	fmt.Println("Rayhan Ahza Widyamukti_109082500210")
+}
